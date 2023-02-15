ed106cdfe1aad0ab83bffd30e3db70c9bc4c997b
Igor Almeida <ofc.iguituu@gmail.com>
2023-02-15 14:32:36 -0300

    Projeto inicial

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const age = 27;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+          backgroundColor: Colors.red,
+        ),
+        body: const Center(
+            child: Card(
+          child: Padding(
+              padding: EdgeInsets.all(32.0), child: Text("Hello World!")),
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
